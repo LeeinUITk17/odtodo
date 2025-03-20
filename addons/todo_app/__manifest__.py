341f58deff6f4ace888ca4c5adad310eff1c9308
tailt-0547 <le.thanh.tai@sun-asterisk.com>
2025-03-20 13:42:29 +0700

    first commit

---
+++ addons/todo_app/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Todo App',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/todo_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
